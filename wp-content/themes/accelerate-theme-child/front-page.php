d5b8f23aa11000d1440b3c2b03c7e9009af97c74
Liam Kirk <liamkirk@192-168-1-3.tpgi.com.au>
2016-02-02 19:32:03 +1000

    Accelerate Deploy take 3

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -12,7 +12,7 @@
  * @since Accelerate Marketing 1.0
  */
 
-get_header(); ?><pre><?php print_r($wp_query); exit; ?></pre>
+get_header(); ?>
 
 <section class="home-page">
 	<div class="site-content">
@@ -24,5 +24,20 @@ get_header(); ?><pre><?php print_r($wp_query); exit; ?></pre>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
+
+<section class="recent-posts">
+	<div class="site-content">
+		<div class="blog-post">
+			<h4>From the Blog</h4>
+            	<?php query_posts('posts_per_page=1'); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+					 <h2><?php the_title(); ?></h2><br  />
+					<?php the_excerpt(); ?>
+					<a href="<?php the_permalink(); ?>" class="read-more-link">Read More <span>&rsaquo;</span></a>
+				<?php endwhile; // end of the loop. ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+		</div>
+	</div>
+</section>
 
 <?php get_footer(); ?>
